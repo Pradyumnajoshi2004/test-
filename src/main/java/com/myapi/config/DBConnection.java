a089c0712f8fc1ccec3307b80d27c8da7eae4e98
Pradyumna <joshipradyumna517@gmail.com>
2026-05-17 19:53:29 +0530

    .

--- src/main/java/com/myapi/config/DBConnection.java
+++ src/main/java/com/myapi/config/DBConnection.java
@@ -22,7 +22,7 @@ public class DBConnection {
         // Fallback for local development
         String host = System.getenv("DB_HOST") != null ? System.getenv("DB_HOST") : "localhost";
         String port = System.getenv("DB_PORT") != null ? System.getenv("DB_PORT") : "3306";
-        String database = System.getenv("DB_NAME") != null ? System.getenv("DB_NAME") : "event_api_db";
+        String database = System.getenv("DB_NAME") != null ? System.getenv("DB_NAME") : "railway";
         String user = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : "root";
         String password = System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : "dBuFCYXSjiJFCUMCnXpcCpaADWcMHmzh";
         
